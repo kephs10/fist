03552147308a484b9717200db85a9772a0d2c067
ndecky <kadiona07@gmail.com>
2020-01-05 17:35:57 +0000

    apres suppression de mon user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,181 +3,174 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
-* @ApiResource()
-* @ORM\Entity(repositoryClass="App\Repository\UserRepository")
-*/
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
 class User implements UserInterface
 {
-/**
-* @ORM\Id()
-* @ORM\GeneratedValue()
-* @ORM\Column(type="integer")
-*/
-private $id;
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
 
-/**
-* @ORM\Column(type="string", length=180, unique=true)
-*/
-private $username;
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
 
-/**
-* @ORM\Column(type="json")
-*/
-private $roles = [];
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
 
-/**
-* @var string The hashed password
-* @ORM\Column(type="string")
-*/
-private $password;
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
 
-/**
-* @ORM\Column(type="string", length=255)
-*/
-private $nomComplet="";
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
 
-/**
-* @ORM\Column(type="string", length=255)
-*/
-private $userName="";
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
 
-/**
-* @ORM\Column(type="string", length=255)
-*/
-private $motPasse="";
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActif;
 
-/**
-* @ORM\Column(type="boolean")
-*/
-private $isActif=true;
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\role")
+     */
+    private $role;
 
-/**
-* @ORM\ManyToOne(targetEntity="App\Entity\role", inversedBy="users")
-*/
-private $role;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-public function getId(): ?int
-{
-return $this->id;
-}
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
 
-/**
-* A visual identifier that represents this user.
-*
-* @see UserInterface
-*/
-public function getUsername(): string
-{
-return (string) $this->username;
-}
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
 
-public function setUsername(string $username): self
-{
-$this->username = $username;
+        return $this;
+    }
 
-return $this;
-}
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
 
-/**
-* @see UserInterface
-*/
-public function getRoles(): array
-{
-$roles = $this->roles;
-// guarantee every user at least has ROLE_USER
-$roles[] = 'ROLE_USER';
+        return array_unique($roles);
+    }
 
-return json_decode($roles);
-}
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
-public function setRoles(array $roles): self
-{
-$this->roles = $roles;
+        return $this;
+    }
 
-return $this;
-}
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
 
-/**
-* @see UserInterface
-*/
-public function getPassword(): string
-{
-return (string) $this->password;
-}
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
 
-public function setPassword(string $password): self
-{
-$this->password = $password;
+        return $this;
+    }
 
-return $this;
-}
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
 
-/**
-* @see UserInterface
-*/
-public function getSalt()
-{
-// not needed when using the "bcrypt" algorithm in security.yaml
-}
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
 
-/**
-* @see UserInterface
-*/
-public function eraseCredentials()
-{
-// If you store any temporary, sensitive data on the user, clear it here
-// $this->plainPassword = null;
-}
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
 
-public function getNomComplet(): ?string
-{
-return $this->nomComplet;
-}
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
 
-public function setNomComplet(string $nomComplet): self
-{
-$this->nomComplet = $nomComplet;
+        return $this;
+    }
 
-return $this;
-}
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
 
-public function getMotPasse(): ?string
-{
-return $this->motPasse;
-}
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
 
-public function setMotPasse(string $motPasse): self
-{
-$this->motPasse = $motPasse;
+        return $this;
+    }
 
-return $this;
-}
+    public function getIsActif(): ?bool
+    {
+        return $this->isActif;
+    }
 
-public function getIsActif(): ?bool
-{
-return $this->isActif;
-}
+    public function setIsActif(bool $isActif): self
+    {
+        $this->isActif = $isActif;
 
-public function setIsActif(bool $isActif): self
-{
-$this->isActif = $isActif;
+        return $this;
+    }
 
-return $this;
-}
+    public function getRole(): ?role
+    {
+        return $this->role;
+    }
 
-public function getRole(): ?role
-{
-return $this->role;
-}
+    public function setRole(?role $role): self
+    {
+        $this->role = $role;
 
-public function setRole(?role $role): self
-{
-$this->role = $role;
-
-return $this;
-}
+        return $this;
+    }
 }
---
+++ src/Migrations/Version20200105172159.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200105172159 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D649D60322AC');
+        $this->addSql('DROP INDEX IDX_8D93D649D60322AC ON user');
+        $this->addSql('ALTER TABLE user DROP role_id, DROP nom_complet, DROP user_name, DROP mot_passe, DROP is_actif');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD role_id INT DEFAULT NULL, ADD nom_complet VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD user_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD mot_passe VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, ADD is_actif TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D649D60322AC FOREIGN KEY (role_id) REFERENCES role (id)');
+        $this->addSql('CREATE INDEX IDX_8D93D649D60322AC ON user (role_id)');
+    }
+}
